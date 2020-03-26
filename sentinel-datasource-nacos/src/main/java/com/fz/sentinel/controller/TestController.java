e47a2fdd624e6f3bb350f0ea6ea1a9e6cca3c437
fangzheng <fangzheng@cfgdc.net>
2020-03-26 23:23:05 +0800

    扩展使用Nacos存储Sentinel规则的类型：限流、降级、热点参数限流、授权、系统

--- sentinel-datasource-nacos/src/main/java/com/fz/sentinel/controller/TestController.java
+++ sentinel-datasource-nacos/src/main/java/com/fz/sentinel/controller/TestController.java
@@ -2,6 +2,7 @@ package com.fz.sentinel.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     @GetMapping("/hello")
-    public String hello() {
-        return "welcome to world!";
+    public String hello(@RequestParam(name="name") String name) {
+        return "welcome to world!" + name;
     }
 }
