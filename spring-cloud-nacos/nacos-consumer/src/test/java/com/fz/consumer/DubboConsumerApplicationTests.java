3ad8792a8f17e5539a76b1e54bcf3b59e266083c
fangzheng <fangzheng@cfgdc.net>
2020-04-01 10:36:34 +0800

    Detail optimization

--- spring-cloud-nacos/nacos-consumer/src/test/java/com/fz/consumer/DubboConsumerApplicationTests.java
+++ spring-cloud-nacos/nacos-consumer/src/test/java/com/fz/consumer/DubboConsumerApplicationTests.java
@@ -1,8 +1,11 @@
 package com.fz.consumer;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DubboConsumerApplicationTests {
 
