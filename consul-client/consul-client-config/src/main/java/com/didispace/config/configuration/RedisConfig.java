fc705337a28f5919c13c2cf7171c77973bca1b85
fangzheng <fangzheng@cfgdc.net>
2020-07-22 11:20:03 +0800

    redisson使用修改

--- consul-client/consul-client-config/src/main/java/com/didispace/config/configuration/RedisConfig.java
+++ consul-client/consul-client-config/src/main/java/com/didispace/config/configuration/RedisConfig.java
@@ -3,8 +3,7 @@ package com.didispace.config.configuration;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -16,9 +15,11 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.*;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import javax.annotation.Resource;
 import java.lang.reflect.Method;
 import java.time.Duration;
 
@@ -112,6 +113,7 @@ public class RedisConfig extends CachingConfigurerSupport {
         .disableCachingNullValues();
 
         RedisCacheManager cacheManager = new RedisCacheManager(writer, config);
+
         return cacheManager;
     }
 
